16d42598642d0508cf96d286658338a2ea737e49
Shuo <shuo@>
2014-05-27 08:08:41 +1000

    made app initialize twitter client

--- config/application.rb
+++ config/application.rb
@@ -1,23 +1,18 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
+require 'twitter'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TwitterSearch
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    def twitter_client
+      @twitter_client ||= Twitter::REST::Client.new do |config|
+        config.consumer_key    = secrets.twitter_consumer_key
+        config.consumer_secret = secrets.twitter_consumer_secret
+      end
+    end
   end
 end
