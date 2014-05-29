9451da90a11fcae332abdc6f306313ce26c59108
Shuo <shuo@>
2014-05-29 11:25:35 +1000

    replaced irb to be pry as default rails console

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,17 @@ Bundler.require(*Rails.groups)
 module TwitterSearch
   class Application < Rails::Application
 
+    console do
+      require 'pry'
+      config.console = Pry
+      unless defined? Pry::ExtendCommandBundle
+        Pry::ExtendCommandBundle = Module.new
+      end
+      require 'rails/console/app'
+      require 'rails/console/helpers'
+      TOPLEVEL_BINDING.eval('self').extend ::Rails::ConsoleMethods
+    end
+
     def twitter_client
       @twitter_client ||= Twitter::REST::Client.new do |config|
         config.consumer_key    = secrets.twitter_consumer_key
